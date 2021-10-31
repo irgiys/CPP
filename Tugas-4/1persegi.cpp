6c61fa58c7f5875406d4bcebcd59e2d678c3ae71
iynsys <irgiyansy@gmail.com>
2021-10-31 20:26:19 +0700

    initial commit

---
+++ Tugas-4/1persegi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int panjang, lebar;
+
+    // PROGRAM
+    cout<<"Masukan nilai panjang = ";
+    cin>>panjang;
+
+    cout<<endl<<"Masukan nilai lebar = ";
+    cin>>lebar;
+
+    cout<<endl;
+    cout<<"Luas persegi adalah = "<<panjang * lebar;
+    return 0;
+}
