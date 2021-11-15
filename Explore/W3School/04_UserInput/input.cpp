fa8e113bf8f2ae6e4565e30a83480a2a12ce2db7
iynsys <irgiyansy@gmail.com>
2021-11-15 11:18:22 +0700

    add 04_UserInput/input.cpp

---
+++ Explore/W3School/04_UserInput/input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num;
+  // cin is pronounced see in 
+  // end use extraction operator morethan twice (>>) 
+  cout << "Input number : " ;cin >> num;
+  cout << endl << "The number is : " << num;
+  return 0;
+}
