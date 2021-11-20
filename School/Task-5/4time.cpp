03fab7ad0494453cc5532f981e011c8b0a7acbe4
iynsys <irgiyansy@gmail.com>
2021-11-20 11:17:16 +0700

    add 4time.cpp

--- School/Task-5/4time.cpp
+++ School/Task-5/4time.cpp
@@ -3,11 +3,34 @@
 using namespace std;
 int main()
 {
-  struct Jam
+    struct Jam
   {
-    /* data */
-    int remainJam, hh, mm, ss;
+    int hh, mm, ss;
   };
   Jam jam;
-}
-// Undone
+  cout << "Masukan Jam : "; cin >> jam.hh;
+  cout << endl << "Masukan Menit : ";  cin >> jam.mm;
+  cout << endl << "Masukan detik : "; cin >> jam.ss;
+
+  cout << endl << "Sebelum  : " << jam.hh << ":" << jam.mm << ":" << jam.ss;
+
+  if(jam.ss < 59){
+    jam.ss = jam.ss + 1;
+  }else{
+    jam.ss = 0;
+    if(jam.mm < 59){
+      jam.mm = jam.mm + 1;
+    }else{
+      jam.mm = 0;
+      if(jam.hh < 24){
+        jam.hh = jam.hh + 1;
+      }else{
+        jam.hh = 0;
+        jam.ss = jam.ss + 1;
+      }
+    }
+  }
+  cout << endl << "Sesudah : " << jam.hh << ":" << jam.mm << ":" << jam.ss;
+  
+  return 0;
+ }
