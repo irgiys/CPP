914bb6959cf100551d2c86719a6006d1aab3ae6a
iynsys <irgiyansy@gmail.com>
2021-11-28 10:11:24 +0700

    String : strnum && length

---
+++ Explore/W3School/07_String/3strnum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+  int x = 12, y =12;
+  cout << endl << x + y;
+
+  string a = "12", b = "12";
+  cout << endl << a + b;
+
+  return 0;
+}
---
+++ Explore/W3School/07_String/4length.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+  string txt = "1inaofubaufoAOBDODBwewOi";
+
+  // use size() / length()
+  cout << "The length of the text is : " << txt.size();
+
+  return 0;
+}
