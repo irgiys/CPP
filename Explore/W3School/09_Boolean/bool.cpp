7fbc624b22ea5219f43f90acf27db0b8c5c74d50
iynsys <irgiyansy@gmail.com>
2021-11-30 09:55:00 +0700

    Boolean : Initial

---
+++ Explore/W3School/09_Boolean/bool.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  bool benar = true, salah = false;
+  
+  cout << endl << "true\t: " << benar;
+  cout << endl << "false\t: " << salah;
+  // expression return a boolean value
+  cout << endl << (2 < 4);
+  cout << endl << (4 < 2);
+}
