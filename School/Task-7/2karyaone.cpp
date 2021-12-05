99396149b36d31de797c98c01193dc2aa554d5fd
iynsys <irgiyansy@gmail.com>
2021-12-05 21:55:16 +0700

    Task-7 : karyaoneUpdated

--- School/Task-7/2karyaone.cpp
+++ School/Task-7/2karyaone.cpp
@@ -9,15 +9,15 @@ int main()
   cout << endl << "Masukan jumlah jam kerja : "; cin >> jam;
   cout << endl << "Masukan Golongan (A|B|C|D) : "; cin >> golongan;
   switch (golongan) {
-    case 'A': upah = 4000;
+    case 'A': case 'a': upah = 4000;
       break;
-    case 'B': upah = 5000;
+    case 'B': case 'b': upah = 5000;
       break;
-    case 'C': upah = 6000;
+    case 'C': case 'c' : upah = 6000;
       break;
-    case 'D': upah = 7500;
+    case 'D': case 'd' : upah = 7500;
       break;
-    default : upah = 0; cout << "Salah golongan" << endl; ;
+    default : upah = 0; cout << "Salah golongan" << endl;
       break;
   }
   if(upah > 1000){
