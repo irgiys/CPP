77d4a38377745e7aba5df8f15dbef5c004a41aac
iynsys <irgiyansy@gmail.com>
2021-12-18 22:37:25 +0700

    Classes : Inheritance

---
+++ Explore/W3School/02_Classes/6inheritance.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Base class 
+class Vehicle {
+  public : 
+    string brand = "Ford";
+    void honk(){
+      cout << "Tuut, tuut" << endl;
+    }
+};
+
+// Derived class 
+class Car: public Vehicle {
+  public :
+    string model = "Mustang";
+};
+
+int main(){
+  Car myCar;
+  myCar.honk();
+  cout << myCar.brand + " " + myCar.model;  
+  cout << endl;
+  return 0;
+}
