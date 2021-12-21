721fe53fa3ea243db507008e7a9d0eeccbae9b48
iynsys <irgiyansy@gmail.com>
2021-12-21 09:09:33 +0700

    Classes : Updated inheritance

--- Explore/W3School/02_Classes/7multiinheritance.cpp
+++ Explore/W3School/02_Classes/7multiinheritance.cpp
@@ -3,13 +3,29 @@ using namespace std;
 
 // Base class (parent)
 class MyClass {
+  protected : // protected access specifier can be accessed in the inherited class
+    int salary;
   public : 
     void myFunction(){
       cout << "Some content in parent class.";
     }
 };
+class MyAnotherClass {
+  public :
+    void myAnotherFunction(){
+      cout << "Some content in another class.";
+    }
+};
 // Derived class (child)
-class MyChild: public MyClass {
+class MyChild: public MyClass , public MyAnotherClass{
+    public :
+      int bonus;
+      void setSalary(int s){
+        salary = s;
+      }
+      int getSalary(){
+        return salary;
+      }
 };
 
 // Derived class (grandchild)
@@ -18,6 +34,11 @@ class MyGrandChild: public MyChild {
 int main(){
   MyGrandChild myObj;
   myObj.myFunction();
+  myObj.setSalary(900000);
+  myObj.bonus = 200000;
+  cout << endl << "Salary " << myObj.getSalary();
+  cout << endl << "Bonus " << myObj.bonus << endl;
+  myObj.myAnotherFunction();
   cout << endl;
   return 0;
 }
