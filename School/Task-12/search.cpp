ead7b5eb96358a46a21b28c9690c5b717f3684f0
iynsys <irgiyansy@gmail.com>
2022-06-17 20:25:47 +0700

    add Task-12

---
+++ School/Task-12/search.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+const int NMAKS = 100;
+typedef int arrayInt[NMAKS];
+void bacaArray(arrayInt A, int n);
+void seqSearch1(arrayInt A, int n, int x, bool *ketemu );
+
+int main(){
+  // DEKLARASI
+  arrayInt A;
+  int n, x;
+  bool found;
+  // ALGORITMA
+  cout << "Inputkan banyaknya n : ";
+  cin >> n;
+  bacaArray(A,n);
+  cout << "Inputkan nilai yang dicari : ";
+  cin >> x;
+  seqSearch1(A,n,x,&found);
+  if (found) {
+    cout << x <<  " ditemukan";
+  } else {
+    cout << x << " tidak ditemukan";
+  }
+  cout << endl;
+  return 0;
+}
+
+void bacaArray(arrayInt A, int n){
+  // DEKLARASI
+  int i;
+  // ALGORITMA
+  for (i = 0; i < n; i++){
+    cout << "Inputkan nilai elemen A["<<i<<"] : "; 
+    cin >> A[i];
+  };
+}
+void seqSearch1(arrayInt A, int n, int x, bool *ketemu){
+  // DEKLARASI
+  int i = 0;
+  while (i < n && A[i] != x ){
+    i = i+1;
+  }
+  if (A[i] == x){
+    *ketemu = true;
+  }else{
+    *ketemu = false;
+  }
+}
